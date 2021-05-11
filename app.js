92c0c89331d311814e8a546da3c4d3f5e01aede2
tailongswayam2000 <66443389+tailongswayam2000@users.noreply.github.com>
2021-05-11 20:29:04 +0530

    added initial files
    Added all initial files

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.render("home", {});
+});
+
+app.get("/plasma", function(req,res){
+  res.render("plasma", {});
+})
+
+app.get("/plasmaRegistration", function(req,res){
+  res.render("plasmaRegistration", {});
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
